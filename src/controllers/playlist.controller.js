ef3f829a9c5e109db5a66852781e1275528e808a
Faizan khan <erfaizankhan04@gmail.com>
2024-07-14 22:16:20 +0530

    playlist controller completed

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,72 +1,148 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Playlist} from "../models/playlist.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
-
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/api_errors.js";
+import { ApiResponse } from "../utils/api_response.js";
+import { asyncHandler } from "../utils/async_handler.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
-    const {name, description} = req.body
-    console.log("name",name);
-    console.log("description",description);
-    //TODO: create playlist
-//    const playlist= await Playlist.create({
-//         name:name,
-//         description:description,
-//         owner:req.user?._id
-//     })
-//     if(!playlist){
-//         throw new ApiError(500,"Error while creating playlist")
-//     }
+  const { name, description } = req.body;
+  console.log("name", name);
+  const playlist = await Playlist.create({
+    name: name,
+    description: description,
+    owner: req.user?._id,
+  });
 
-    return res.status(201).json(new ApiResponse(200,{},"Playlist created successfully"))
-})
+  if (!playlist) {
+    throw new ApiError(500, "Some error occured while creating playlist");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, playlist, "Playlist created successfully"));
+});
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
-    const {userId} = req.params
-    //TODO: get user playlists
-    const playlist=await Playlist.findById(userId);
-    console.log(playlist);
+  const { userId } = req.params;
+  console.log("userId", userId);
 
-    if(!playlist){
-        throw new ApiError(400,"Some error occured while fetching playlists");
-    }
+  const playlists = await Playlist.find({ owner: userId }).select(
+    "-videos -updatedAt -__v"
+  );
 
-    return res.status(200).json(new ApiResponse(200,playlist,"playlist fetched successfully"))
-})
+  if (!playlists) {
+    throw ApiError(500, "Some error occured while fetching playlists");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlists, "Playlists fetched successfully"));
+  //TODO: get user playlists
+});
 
 const getPlaylistById = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    //TODO: get playlist by id
-})
+  const { playlistId } = req.params;
+  //TODO: get playlist by id
+  const playlists = await Playlist.findById(playlistId).select("-__v");
+
+  if (!playlists) {
+    throw ApiError(500, "Some error occured while fetching given playlist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlists, "Required Playlist fetched successfully")
+    );
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-})
+  const { playlistId, videoId } = req.params;
+
+  const addVideoToPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $push: { videos: videoId },
+    },
+    { new: true }
+  );
+
+  if (!addVideoToPlaylist) {
+    throw new ApiError(
+      500,
+      "Some error occured while adding video in playlist"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Successfully video added in playlist"));
+});
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+  const { playlistId, videoId } = req.params;
+  // TODO: remove video from playlist
+  const removeVideoToPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $pull: { videos: videoId },
+    },
+    { new: true }
+  );
 
-})
+  if (!removeVideoToPlaylist) {
+    throw new ApiError(
+      500,
+      "Some error occured while removing video in playlist"
+    );
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Successfully video removed in playlist"));
+});
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    // TODO: delete playlist
-})
+  const { playlistId } = req.params;
+  // TODO: delete playlist
+});
 
 const updatePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
-    const {name, description} = req.body
-    //TODO: update playlist
-})
+  const { playlistId } = req.params;
+  const { name, description } = req.body;
+  //TODO: update playlist
+  const updatePlaylistResponse = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $set: {
+        name: name,
+        description: description,
+      },
+    },
+    { new: true }
+  );
+
+  if (!updatePlaylistResponse) {
+    throw new ApiError(500, "Error while updating playlist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatePlaylistResponse,
+        "playlist updated successfully"
+      )
+    );
+});
 
 export {
-    createPlaylist,
-    getUserPlaylists,
-    getPlaylistById,
-    addVideoToPlaylist,
-    removeVideoFromPlaylist,
-    deletePlaylist,
-    updatePlaylist
-}
+  createPlaylist,
+  getUserPlaylists,
+  getPlaylistById,
+  addVideoToPlaylist,
+  removeVideoFromPlaylist,
+  deletePlaylist,
+  updatePlaylist,
+};
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import { addVideoToPlaylist, createPlaylist, getPlaylistById, getUserPlaylists, removeVideoFromPlaylist, updatePlaylist } from "../controllers/playlist.controller.js";
 
 const router=Router()
 
 router.use(verifyJWT)
 
 router.route("/create-playlist").post(createPlaylist)
+router.route("/get-playlists/:userId").get(getUserPlaylists)
+router.route("/get-playlist-by-id/:playlistId").get(getPlaylistById)
+router.route("/add-video-to-playlist/:playlistId/:videoId").patch(addVideoToPlaylist)
+router.route("/remove-video-to-playlist/:playlistId/:videoId").patch(removeVideoFromPlaylist)
+router.route("/update-playlist/:playlistId").patch(updatePlaylist)
+
+export default router;
